8fd512e495c032879bef8280ac563fe7ca50d8d1
alexortizestech <alejandro.ortiz@escuelaestech.es>
2021-07-27 10:41:58 +0200

    2nd commit
    Player movement and Point & Click System

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    public Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButton(0))
+        {
+            target = new Vector2(mouse.x, transform.position.y);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
+    }
+}
---
+++ Assets/Scripts/PointandClickRay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PointandClickRay : MonoBehaviour
+{
+    public float Length;
+    public LayerMask Layer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit Hit;
+            Ray Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(Ray, out Hit, Length, Layer))
+            {
+                Debug.Log(Hit.collider.name);
+            }
+        }
+    }
+}
