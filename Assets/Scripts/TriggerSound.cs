b791d983ab93969b70c0aa78eb5f893ff8947038
Alejandro Ortiz <alejandro.ortiz@escuelaestech.es>
2021-07-29 00:52:48 +0200

    25th commit

---
+++ Assets/Scripts/TriggerSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour
+{
+    public bool isPlaying;
+    public AudioSource Audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlaying = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (!isPlaying)
+            {
+
+                 Audio.Play();
+                 isPlaying = true;
+            }
+        }
+    }
+}
