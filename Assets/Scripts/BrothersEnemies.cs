c2798b9830c91c2ad3895ec58a54653007b19eab
alexortizestech <alejandro.ortiz@escuelaestech.es>
2021-07-28 12:20:19 +0200

    16th commit
    Finishing lvl03

---
+++ Assets/Scripts/BrothersEnemies.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrothersEnemies : MonoBehaviour
+{
+    public WinLose WinLose;
+    public bool isPlayer;
+    public bool isEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPlayer)
+        {
+            WinLose.Lose();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPlayer = true;
+        }
+
+    }
+}
