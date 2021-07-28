d3b20f567614c586f185ef34c98c55731f67a61a
Alejandro Ortiz <alejandro.ortiz@escuelaestech.es>
2021-07-28 18:19:46 +0200

    22nd commit
    Player

---
+++ Assets/Scripts/TriggerEnabler2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEnabler2 : MonoBehaviour
+{
+    public Level02 LVL2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            LVL2.enabled = true;
+
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        LVL2.enabled = false;
+    }
+}
